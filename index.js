87f154cdcfacd453c9e86edf734ecf72d812b44b
samiul-hasan-sysforge <samiulh085@gmail.com>
2026-05-19 11:14:17 +0600

    added booking post api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.MONGODB_URI;
 const port = process.env.PORT || 5000;
 
@@ -19,17 +19,28 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
     try {
-        // await client.connect();
         const database = client.db(process.env.DB_NAME);
         const facilitiesCollection = database.collection(process.env.ALL_COLLECTION);
+        const bookingCollection = database.collection(process.env.ALL_BOOKING)
 
         app.get('/facilities', async (req, res) => {
             const cursor = facilitiesCollection.find()
             const result = await cursor.toArray()
             res.send(result)
         })
+        app.get('/facilities/:id', async (req, res) => {
+            const id = req.params.id
+            const user = await facilitiesCollection.findOne({ _id: new ObjectId(id) })
+            res.send(user)
+        })
 
-        // await client.db("admin").command({ ping: 1 });
+        app.post('/bookings', async (req, res) => {
+            const bookingData = req.body
+            const result = await bookingCollection.insertOne(bookingData)
+            res.json(result)
+        })
+
+
         console.log("MongoDB connected 🚀");
     } finally {
         // await client.close();
