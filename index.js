3f91a39f6ce66a61478fb564edc79b3eff60e304
samiul-hasan-sysforge <samiulh085@gmail.com>
2026-05-20 17:16:00 +0600

    added verifyToken function to verify for all api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 const uri = process.env.MONGODB_URI;
 const port = process.env.PORT || 5000;
 
@@ -17,13 +18,39 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+const JWKS = createRemoteJWKSet(
+    new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+)
+
+const verifyToken = async (req, res, next) => {
+    const header = req?.headers.authorization
+    if (!header) {
+        res.status(401).json({ message: "Unauthorized" })
+    }
+    const token = header.split(' ')[1]
+
+    if (!token) {
+        res.status(401).json({ message: "Unauthorized" })
+    }
+
+    try {
+        const { payload } = await jwtVerify(token, JWKS)
+        console.log(payload)
+        next()
+    } catch (error) {
+        res.status(401).json({ message: "Forbidden" })
+    }
+}
+
+
 const run = async () => {
     try {
         const database = client.db(process.env.DB_NAME);
         const facilitiesCollection = database.collection(process.env.ALL_COLLECTION);
         const bookingCollection = database.collection(process.env.ALL_BOOKING)
 
-        app.get('/facilities', async (req, res) => {
+        app.get('/facilities', verifyToken, async (req, res) => {
             const { search } = req.query
             let cursor
 
@@ -50,32 +77,29 @@ const run = async () => {
             }
 
             const result = await cursor.toArray()
-            console.log(result)
             res.send(result)
         })
 
-        app.get('/facilities/:id', async (req, res) => {
+        app.get('/facilities/:id', verifyToken, async (req, res) => {
             const id = req.params.id
             const user = await facilitiesCollection.findOne({ _id: new ObjectId(id) })
             res.send(user)
         })
 
-        app.post('/facilities', async (req, res) => {
+        app.post('/facilities', verifyToken, async (req, res) => {
             const facilitiesData = req.body
             const result = await facilitiesCollection.insertOne(facilitiesData)
-            console.log(result)
             res.json(result)
         })
 
 
-        app.get('/facilities/user/:userId', async (req, res) => {
+        app.get('/facilities/user/:userId', verifyToken, async (req, res) => {
             const id = req.params.userId
-            console.log(id)
             const user = await facilitiesCollection.find({ userId: id }).toArray()
             res.send(user)
         })
 
-        app.patch('/facilities/user/:userId', async (req, res) => {
+        app.patch('/facilities/user/:userId', verifyToken, async (req, res) => {
             const { userId } = req.params
             const updatedData = req.body
             const result = await facilitiesCollection.updateOne(
@@ -85,25 +109,25 @@ const run = async () => {
             res.json(result)
         })
 
-        app.delete('/facilities/:userId', async (req, res) => {
+        app.delete('/facilities/:userId', verifyToken, async (req, res) => {
             const { userId } = req.params
             const result = await facilitiesCollection.deleteOne({ userId })
             res.json(result)
         })
 
-        app.get('/bookings/:userEmail', async (req, res) => {
+        app.get('/bookings/:userEmail', verifyToken, async (req, res) => {
             const { userEmail } = req.params
             const result = await bookingCollection.find({ user_email: userEmail }).toArray()
             res.json(result)
         })
 
-        app.post('/bookings', async (req, res) => {
+        app.post('/bookings', verifyToken, async (req, res) => {
             const bookingData = req.body
             const result = await bookingCollection.insertOne(bookingData)
             res.json(result)
         })
 
-        app.delete('/bookings/:bookingId', async (req, res) => {
+        app.delete('/bookings/:bookingId', verifyToken, async (req, res) => {
             const bookingId = req.params.bookingId
             const result = await bookingCollection.deleteOne({ _id: new ObjectId(bookingId) })
             res.json(result)
